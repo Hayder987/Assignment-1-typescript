8a2ba8a10a9f8715041a40791ff390e0c6a4485e
hayder <hayderbd4290@gmail.com>
2025-05-07 21:00:12 +0600

    solve problem 2

--- src/assignment.ts
+++ src/assignment.ts
@@ -6,7 +6,26 @@
       return input.toLocaleLowerCase();
     }
   }
-  
+
+
+  function filterByRating(
+    items: { title: string; rating: number }[]
+  ): { title: string; rating: number }[] {
+    {
+      const higherRating = items.filter((item) => item.rating >= 4);
+      return higherRating;
+    }
+  }
 
   
+  
+
+
+
+
+
+
+
+
+
 }
