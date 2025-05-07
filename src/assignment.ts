59cdf075cc4ebd7dc8c8af5b5d7a94f50d5cdfc2
hayder <hayderbd4290@gmail.com>
2025-05-07 21:45:15 +0600

    solve problem 4

--- src/assignment.ts
+++ src/assignment.ts
@@ -16,13 +16,35 @@
       return higherRating;
     }
   }
-  
+
 
   function concatenateArrays<T>(...arrays: T[][]): T[] {
-    const mergeArray = arrays.reduce((acc, curr) => acc.concat(curr), [] as T[]);
+    const mergeArray = arrays.reduce((merged, currentArray) => merged.concat(currentArray), [] as T[]);
     return mergeArray
   }
+
+
+ 
+  class Vehicle {
+    constructor(private make: string, private year: number) {
+    }
+    getInfo(): string {
+      return `Make: ${this.make}, Year: ${this.year}`;
+    }
+  }
   
+  class Car extends Vehicle {
+  constructor(make: string, year: number, private model: string) {
+      super(make, year);  
+    }
+  
+    getModel(): string {
+      return `Model: ${this.model}`;
+    }
+  }
+
+
+
  
   
 
