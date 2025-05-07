addd0bf734391267f517768c1de2cc2f4b033653
hayder <hayderbd4290@gmail.com>
2025-05-07 22:04:38 +0600

    solve problem 6

--- src/assignment.ts
+++ src/assignment.ts
@@ -49,7 +49,25 @@
     }
   }
 
- 
+
+  interface Product {
+    name: string;
+    price: number;
+  }
+  
+  function getMostExpensiveProduct(products: Product[]): Product | null {
+    if (products.length === 0) {
+      return null;
+    }
+  
+    return products.reduce((maxProduct, currentProduct) =>
+      currentProduct.price > maxProduct.price ? currentProduct : maxProduct
+    );
+  }
+
+
+  
+  
 
 
 
