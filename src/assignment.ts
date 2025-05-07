28cee296beaaf5de8c435b28d0827c8cec638f7f
hayder <hayderbd4290@gmail.com>
2025-05-07 20:38:42 +0600

    solve number 1 problem

--- src/assignment.ts
+++ src/assignment.ts
@@ -1,19 +1,12 @@
 {
+  function formatString(input: string, toUpper: boolean = true): string {
+    if (toUpper) {
+      return input.toUpperCase();
+    } else {
+      return input.toLocaleLowerCase();
+    }
+  }
+  
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
+  
 }
